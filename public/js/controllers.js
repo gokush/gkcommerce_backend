472f8a7ad95ea53a66dadfb8eea2779cff149bbc
goku <kakarrot.sh@qq.com>
2014-12-30 20:47:41 +0800

    提交表单

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,10 +1,15 @@
 var gkcommerceController = angular.module('gkcommerceControllers', [])
-gkcommerceController.controller('OAuthLoginCtrl', ['$scope',
-function($scope) {
+gkcommerceController.controller('OAuthLoginCtrl', ['$scope', '$http',
+function($scope, $http) {
     $scope.login = function() {
         $scope.$broadcast('show-errors-check-validity')
         if ($scope.loginForm.$valid) {
-
+            console.log($scope.user)
+            $http.
+                post("/api/oauth/login", $scope.user).
+                success(function(data, status, headers, config) {
+                    console.log(data)
+                })
         }
     }
 }])
---
+++ public/js/services.js
@@ -0,0 +1,4 @@
+var gokuServices = angular.module('gokuServices', ['ngResource'])
+gokuService.factory('Address', ['$resource',
+function($resource) {
+}])
